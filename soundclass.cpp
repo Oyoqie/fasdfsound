1e3b14a8cf1ec567291cd249228e4ff70e52ea5e
Yoqie <44141075+Oyoqie@users.noreply.github.com>
2020-08-05 16:08:49 +0300

    Update soundclass.cpp

--- soundclass.cpp
+++ soundclass.cpp
@@ -1,11 +1,14 @@
 #include <soundclass.h>
 
-BYTE* chk_wavfile::LoadData(const size_t n) {
-	Data = (BYTE*)malloc(n);
-	return Data;
+chk_wavfile::chk_wavfile() : Data(NULL) {
 }
 
 chk_wavfile::~chk_wavfile() {
 	if (Data)
 		free(Data);
+}
+
+BYTE* chk_wavfile::LoadData(const size_t n) {
+	Data = (BYTE*)malloc(n);
+	return Data;
 }
